e5565ae85dd5745e7e301265f23673e6b51c175f
Raffaela Castro <raffacst@outlook.com>
2024-11-26 17:00:10 -0300

    feat: :heavy_plus_sign: add ScreenUtilInit for app responsiveness
    add ScreenUtilInit for app responsiveness
    
    add ScreenUtilInit for app responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:chat_online/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ChatOnline());
 }
 
@@ -9,9 +14,10 @@ class ChatOnline extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Center(
-      child: Text("Welcome to Chat Online"),
-    ));
+    return ScreenUtilInit(
+        builder: (context, child) => const MaterialApp(
+                home: Center(
+              child: Text("Welcome to Chat Online"),
+            )));
   }
 }
